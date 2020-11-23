089a540371b6f4743945e9a186c95aa387430215
anyappscompany <56433722+anyappscompany@users.noreply.github.com>
2020-11-23 20:41:15 +0200

    Initial commit

---
+++ app/src/main/java/ua/com/anyapps/kt_nav_component/MainActivity.kt
@@ -0,0 +1,25 @@
+package ua.com.anyapps.kt_nav_component
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment??:return
+        val navController = host.navController
+
+
+
+        navController.navigate(R.id.blankFragment3)
+
+    }
+}
